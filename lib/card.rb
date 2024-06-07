9e011edd1aec8a83da5637c5250d9dafd87b980a
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-06-07 10:15:04 -0400

    Added tests that pass for initialize class and compare cards

---
+++ lib/card.rb
@@ -0,0 +1,15 @@
+class Card
+  attr_reader :suit, :rank, :numerical_rank
+
+  RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+    @numerical_rank = RANKS.index(rank)
+  end
+
+  def ==(other)
+    rank == other.rank
+  end
+end
---
+++ spec/card_spec.rb
@@ -0,0 +1,33 @@
+# spec/card_spec.rb
+
+require_relative '../lib/card'
+
+RSpec.describe Card do
+  describe '#initialize' do
+    before do
+      @card = Card.new('2', 'H')
+    end
+
+    it 'responds to suit, rank, and numerical rank' do
+      expect(@card).to respond_to(:suit)
+      expect(@card).to respond_to(:rank)
+      expect(@card).to respond_to(:numerical_rank)
+    end
+  end
+
+  describe '#==' do
+    before do
+      @card1 = Card.new('2', 'H')
+      @card2 = Card.new('2', 'C')
+      @card3 = Card.new('3', 'H')
+    end
+
+    it 'returns true if the cards have the same rank' do
+      expect(@card1 == @card2).to be true
+    end
+
+    it 'returns false if the cards have different ranks' do
+      expect(@card1 == @card3).to be false
+    end
+  end
+end
